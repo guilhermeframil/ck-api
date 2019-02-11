bf9e08f6288bc416ac14f1e7c0341371ea8aadf9
[guilherme] <[guilherme.framil@accenture.com]>
2019-02-11 08:31:32 -0200

    initial commit

---
+++ ck-api/src/main/java/com/ckapi/CkApiApplication.java
@@ -0,0 +1,14 @@
+package com.ckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkApiApplication.class, args);
+	}
+
+}
+
