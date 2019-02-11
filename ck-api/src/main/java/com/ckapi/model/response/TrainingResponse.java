9dfd9979d01d7116ea6ed1ec9fb8a9ba71533cd2
[guilherme] <[guilherme.framil@accenture.com]>
2019-02-11 09:38:23 -0200

    Criação do model response de treinamentos

---
+++ ck-api/src/main/java/com/ckapi/model/response/TrainingResponse.java
@@ -0,0 +1,98 @@
+package com.ckapi.model.response;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class TrainingResponse {
+
+	private String name;
+	private String duration;
+	private String place;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private LocalDate date;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+	private LocalDate time;
+	private String leader;
+	private String url;
+	private String Links;
+
+	public TrainingResponse() {
+		super();
+	}
+
+	public TrainingResponse(String name, String duration, String place, LocalDate date, LocalDate time, String leader) {
+		super();
+		this.name = name;
+		this.duration = duration;
+		this.place = place;
+		this.date = date;
+		this.time = time;
+		this.leader = leader;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public LocalDate getTime() {
+		return time;
+	}
+
+	public void setTime(LocalDate time) {
+		this.time = time;
+	}
+
+	public String getLeader() {
+		return leader;
+	}
+
+	public void setLeader(String leader) {
+		this.leader = leader;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getLinks() {
+		return Links;
+	}
+
+	public void setLinks(String links) {
+		Links = links;
+	}
+
+}
